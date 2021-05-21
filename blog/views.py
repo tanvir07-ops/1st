6a4a5d9fe7d4a6384c27d6624ef0c865ccb50abd
tanvir07-ops <tanvir9836859@gmail.com>
2021-05-21 04:40:28 -0700

    I commit this 1st time

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def starting_page(request):
+    return render(request,'blog/index.html',context={})
