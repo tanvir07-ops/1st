8b0ad362e3192297dd622132158e63f6e9e3be5f
tanvir07-ops <tanvir9836859@gmail.com>
2021-05-21 05:05:34 -0700

    I commit wsgi and settings

--- my_site/settings.py
+++ my_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*poy&ct-81lgsgnl4$cc(ac4ydce3+0$=pqi8c)c%zzliqbtb+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,4 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
